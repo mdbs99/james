b5849333f4c3571d780db4be9a440cbd4ad5118c
Nuno Picado <geral@nunopicado.com>
2017-07-21 00:08:14 +0100

    #57 Created interfaces for the james testing unit

---
+++ test/james.testing.pas
@@ -0,0 +1,20 @@
+unit james.testing;
+
+{$include james.inc}
+
+interface
+
+type
+  ITest = interface
+  ['{CF6EE529-CC09-461F-B6FB-526982D37C3B}']
+    function RegisterTest(const SuitePath: string): ITest;
+  end;
+
+  ITestSuite = interface
+  ['{08E6BA19-7082-4BC0-AA14-8E7A92633D5B}']
+    function Add(const Test: ITest): ITestSuite;
+  end;
+
+implementation
+
+end.
