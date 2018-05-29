b7bd6818409862b5d0f40722bc666ab059375826
Marcos Douglas <mdbs99@delfire.net>
2018-05-29 00:13:05 -0300

    #89 tests close #89

---
+++ test/james.core.tests.pas
@@ -0,0 +1,83 @@
+{
+  MIT License
+
+  Copyright (c) 2017-2018 Marcos Douglas B. Santos
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+} 
+unit James.Core.Tests;
+
+{$i James.inc}
+
+interface
+
+uses
+  Classes, SysUtils, COMObj, Variants,
+  James.Data.Base,
+  James.Data.Clss,
+  James.Testing.Clss,
+  James.Core.Adapters;
+
+type
+  TOleVariantAsStreamTest = class(TTestCase)
+  published
+    procedure TestValue;
+  end;
+
+  TOleVariantFromStreamTest = class(TTestCase)
+  published
+    procedure TestValue;
+  end;
+
+implementation
+
+{ TOleVariantAsStreamTest }
+
+procedure TOleVariantAsStreamTest.TestValue;
+var
+  V: OleVariant;
+begin
+  V := TOleVariantFromStream.New(TDataStream.New('foo')).Value;
+  CheckEquals(
+    'foo',
+    TOleVariantAsStream.New(V).Value.AsString
+  );
+end;
+
+{ TOleVariantFromStreamTest }
+
+procedure TOleVariantFromStreamTest.TestValue;
+var
+  S: IDataStream;
+begin
+  S := TDataStream.New('foo');
+  CheckEquals(
+    'foo',
+    TOleVariantAsStream.New(
+      TOleVariantFromStream.New(S).Value
+    ).Value.AsString
+  );
+end;
+
+initialization
+  TTestSuite.New('Core')
+    .Add(TTest.New(TOleVariantAsStreamTest))
+    .Add(TTest.New(TOleVariantFromStreamTest))
+
+end.
